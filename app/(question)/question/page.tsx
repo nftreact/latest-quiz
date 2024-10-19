a53d1530ea139a2c5a8182863e679d0fc893b687
Ali Hasani <hasaninft1992@gmail.com>
2024-10-19 21:07:22 +0330

    fix: change remote file

--- app/(question)/question/page.tsx
+++ app/(question)/question/page.tsx
@@ -88,6 +88,7 @@ export default function LandingPage({ searchParams }: { searchParams: FormData }
     setValue('isLoading', isFetching)
   }, [isFetching])
 
+  
   if (!data) return <QuestionLoading />
 
   /**
